445ecf0a84c286b20f5c4dfb073d42804f53dad5
Ashen Udithamal <udithamal.lk@gmail.com>
2024-02-10 19:51:07 +0530

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import ListGroup from './components/ListGroup';
 import Alert from './components/Alert';
+import Button from './components/Button';
 
 function App() {
     let countriesList = ['India', 'Sri Lanka', 'Japan', 'Australia', 'Canada'];
@@ -17,6 +18,9 @@ function App() {
             <Alert>
                 Hello <span>World</span>
             </Alert>
+            <Button onClick={() => console.log('clicked')}>
+                Click Here
+            </Button>
             <ListGroup heading='Countries' items={countriesList} onSelectItem={handleSelectItem} />
         </div>
     );
